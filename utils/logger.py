97904a99fb0f40d3966f76bdb010bf8890645319
naman.goel <naman.goel@groundzero.cloud>
2025-10-02 22:30:46 +0530

    cleanup

--- utils/logger.py
+++ utils/logger.py
@@ -1,14 +1,3 @@
-# import logging
-
-# def get_logger():
-    # logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    # logger = logging.getLogger('uvicorn')
-    # console_handler = logging.StreamHandler()
-    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(path)s - %(message)s')
-    # console_handler.setFormatter(formatter)
-    # logger.addHandler(console_handler)
-    # logger.setLevel(logging.INFO)
-    # return logger
 import logging
 
 def get_console_logger(name="repliq"):
@@ -23,7 +12,6 @@ def get_console_logger(name="repliq"):
         logger.setLevel(logging.INFO)
     return logger
 
-# logger = get_logger("repliq")
 
 def get_file_logger(name="repliq", log_file="app.log"):
     logger = logging.getLogger(name)
@@ -39,19 +27,3 @@ def get_file_logger(name="repliq", log_file="app.log"):
         logger.setLevel(logging.INFO)
     
     return logger
-
-# import logging
-# # Create a custom logger
-# logger = logging.getLogger(__name__)
-# # Create handlers
-# console_handler = logging.StreamHandler()
-# # Create formatters and add them to handlers
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# console_handler.setFormatter(formatter)
-# # Add handlers to the logger
-# logger.addHandler(console_handler)
-# # Set the logging level
-# logger.setLevel(logging.INFO)
-# # Log messages
-# logger.info("This is an info message")
-# logger.warning("This is a warning message")
